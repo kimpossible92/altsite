af7486c0a4e5d65724330896d673f8e71a9d2ef1
kimpossible92 <kim.possible.92@bk.ru>
2022-06-30 11:59:34 +0300

    sdsd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, {useState} from 'react';
-import Counter from "./components/Cntr";
-import ClassCounter from "./components/ClassCounter";
-function App() {
-  const [value,setValue] = useState('4');
-  return (
-    <div className="App">
-        <Counter/>
-        <ClassCounter/>
-        <Counter/>
-        <h1>{value}</h1>
-        <input
-            type="text"
-            value={value}
-            onChange={event => setValue(event.target.value)}
-        />
-    </div>
-  );
-}
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
 
-export default App;
+const unityContext = new UnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+    webGLContextAttributes: {
+        alpha: true,
+        antialias: true,
+        depth: true,
+        failIfMajorPerformanceCaveat: true,
+        powerPreference: "high-performance",
+        premultipliedAlpha: true,
+        preserveDrawingBuffer: true,
+        stencil: true,
+        desynchronized: true,
+        xrCompatible: true,
+    },
+});
+
+function App() {
+    return <Unity unityContext={unityContext} />;
+}
